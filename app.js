4a0332ae5bc98bc9d0d46d09725b6c9725755fc9
mabooe <mabooe@gmail.com>
2020-01-26 14:14:08 -0800

    prepare for heroku push

--- app.js
+++ app.js
@@ -8,12 +8,20 @@ const maps = require("./routes/api/maps");
 const User = require('./models/User');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 //added for sockets
 const io = require("socket.io");
 const chatServer = io.listen(7000);
 
-// 
+//
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+  }
 
 mongoose
     .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
